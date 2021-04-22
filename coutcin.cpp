c73ef8fe80bbc6da77819fc8ad4b61654ea9ba06
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-04-22 13:21:42 +0530

    Add files via upload

---
+++ coutcin.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int x,y;
+cout<<"Enter x and y values: ";
+cin>>x>>y;
+cout<<"add"<<(x+y);
+return 0;
+}
---
+++ cppcode1.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+class vit;
+{
+    int x,y;
+    public;
+    void add()
+    {
+        x=1;
+        y=2;
+        cout<<"sum of : "<<(x+y);
+    }
+};
+int main()
+{
+    vit a;
+    a.add();
+    return 0;
+
+}
