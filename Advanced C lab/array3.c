201dd33df7e0692d3fe5b2c1c415cf012da4d0d1
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2023-01-21 23:04:29 +0530

    Add files via upload

---
+++ Advanced C lab/array3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char name[30];
+    printf("Enter name: ");
+    gets(name);     // read string
+    printf("Name: ");
+    puts(name);    // display string
+    return 0;
+}
+
